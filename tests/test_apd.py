6fe588ea915d65fdab00b53f883b0a72ef6cf564
John Barbuto <john.barbuto@venmo.com>
2015-06-06 14:23:05 -0700

    Switch to pytest fixture and generate artifact for circle ci

--- tests/test_apd.py
+++ tests/test_apd.py
@@ -1,14 +1,26 @@
 import json
+import os
+
+import pytest
 
 from sforparser.apd import scraper
 
 INPUT_FILE = 'data/apd/input.txt'
 
 
-def test_output_strips_email_spaces():
+@pytest.fixture
+def data():
     json_str = scraper(open(INPUT_FILE))
 
-    data = json.loads(json_str)
+    artifact_dir = os.getenv('CIRCLE_ARTIFACTS')
+    if artifact_dir:
+        artifact_file = os.path.join(artifact_dir, 'apd.json')
+        open(artifact_file, 'w').write(json_str)
+
+    return json.loads(json_str)
+
+
+def test_output_strips_email_spaces(data):
     offensive_field = data[70]["locations"][0]["emails"]
     expected = [
         "ronald.sanders@sfdph.org",
